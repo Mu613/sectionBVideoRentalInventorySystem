bfd28726d1ac0696e5c07f2cdfb19a0beaa4e527
Mu613 <muskan.bhardwaj_cs19@gla.ac.in>
2020-08-19 16:32:46 +0530

    add Video.java

---
+++ src/definitions/Video.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Muskan Bhardwaj
+ *   Date: 19-08-2020
+ *   Time: 04:30 PM
+ *   File: Video.java
+ */
+
+package definitions;
+
+public class Video {
+}
